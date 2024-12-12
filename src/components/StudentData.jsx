350e16a50ebb91a35b97e8e63eda408a34048982
siddharthVoltus <v.siddhartha@voltuswave.com>
2024-12-12 10:56:49 +0530

    updated sign out url -3

--- src/components/StudentData.jsx
+++ src/components/StudentData.jsx
@@ -31,11 +31,7 @@ export default function StudentData() {
   };
 
   const signOutRedirect = () => {
-    const clientId = "44atgttl25bga3nahgbvgf54kv";
-    const logoutUri = "<logout uri>";
-    const cognitoDomain =
-      "https://eu-north-1syo4lgo3t.auth.eu-north-1.amazoncognito.com";
-    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+    navigation("/student-portal");
     auth.removeUser();
   };
   const signOut = () => signOutRedirect();
